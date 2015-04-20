93dd275bbca47f2e987fec397f4991999b0705e4
plyapunov <plyapunov@yahoo.com.ar>
2015-04-20 00:37:15 -0300

    TP1
    Lyapunov Pylyp
    k2051

---
+++ TP1.c
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : TP1.c
+// Author      : Lyapunov Pylyp
+// Version     : 20/04/2015
+//============================================================================
+
+#include <stdio.h>
+
+using namespace std;
+
+int main () { 
+	int celcius;
+	
+	for (celcius = 0;celcius<=60;celcius = celcius + 10)
+		printf("%3d %6.1f/n", celcius , celcius*(5.0/9.0) + 32);
+		
+}
